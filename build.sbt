f98cb6de2ebe7b28729f1b9f09e003b43690f1f4
Guillaume <guillaume.bogard@cpexterne.org>
2020-01-10 11:20:10 +0100

    Updated CI

--- build.sbt
+++ build.sbt
@@ -1,19 +1,20 @@
 import sbtghpackages.TokenSource.Environment
 
 ThisBuild / scalaVersion := "2.12.10"
-ThisBuild / version := "0.1.0"
+ThisBuild / version := "0.3.1"
 ThisBuild / organization := "lambda"
 ThisBuild / organizationName := "Lambdacademy"
-ThisBuild / fork := true
+
+ThisBuild / githubUser := sys.env.getOrElse("GITHUB_USER", "REPLACE_ME")
 ThisBuild / githubOwner := "lambdacademy-dev"
 ThisBuild / githubTokenSource := Some(Environment("GITHUB_TOKEN"))
-ThisBuild / githubUser := sys.env.getOrElse("GITHUB_USER", "REPLACE_ME")
+ThisBuild / githubRepository := "program-executor"
 
 lazy val root = (project in file("."))
   .settings(
     name := "program-executor",
+    githubOwner := "lambdacademy-dev",
     libraryDependencies ++= Seq(
-      "com.zaxxer" % "nuprocess" % "1.2.5",
       "co.fs2" %% "fs2-core" % "2.1.0"
     )
   )
--- src/main/scala/lambda/programexecutor/package.scala
+++ src/main/scala/lambda/programexecutor/package.scala
@@ -1,60 +1,49 @@
 package lambda
 
-import java.nio.ByteBuffer
-import java.nio.charset.StandardCharsets
-
+import cats.implicits._
+import cats.data.EitherT
 import cats.effect.IO
-import com.zaxxer.nuprocess.{NuAbstractProcessHandler, NuProcess, NuProcessBuilder}
 import fs2._
 import fs2.concurrent.Queue
 import lambda.programexecutor.Executor._
 import lambda.programexecutor.ProgramEvent.{Exit, StdErr, StdOut}
 
+import scala.sys.process._
+
 package object programexecutor {
 
-
-  def runProcess(commands: List[String], destroy: IO[Unit] = IO.unit): IO[Stream[IO, ProgramEvent]] = {
-
-    for {
+  def runProcess(commands: List[String]): Stream[IO, ProgramEvent] = {
+    val streamIO = for {
       queue <- Queue.unbounded[IO, Option[ProgramEvent]]
-      _ <- IO
-        .cancelable[Unit] { cb =>
-          val processHandler = new NuAbstractProcessHandler {
-
-            override def onStart(nuProcess: NuProcess): Unit = {
-              cb(Right())
-            }
-
-            override def onExit(statusCode: Int): Unit = {
-              queue.offer1(Some(Exit(statusCode)))
-              queue.offer1(None)
-            }
-
-            override def onStdout(buffer: ByteBuffer, closed: Boolean): Unit = {
-              val bytes = new Array[Byte](buffer.remaining)
-              buffer.get(bytes)
-              queue
-                .offer1(Some(StdOut(new String(bytes, StandardCharsets.UTF_8))))
-                .unsafeRunAsyncAndForget()
-            }
-
-            override def onStderr(buffer: ByteBuffer, closed: Boolean): Unit = {
-              val bytes = new Array[Byte](buffer.remaining)
-              buffer.get(bytes)
-              queue
-                .offer1(Some(StdErr(new String(bytes, StandardCharsets.UTF_8))))
-                .unsafeRunAsyncAndForget()
-            }
-
+      _ <- (IO {
+        val processLogger = new ProcessLogger {
+          def out(s: => String): Unit = {
+            queue.offer1(Some(StdOut(s))).unsafeRunAsyncAndForget()
           }
-          val pb = new NuProcessBuilder(commands: _*)
-          pb.setProcessListener(processHandler)
-          val process = pb.start()
 
-          destroy.flatMap(_ => IO(process.destroy(true)))
+          def err(s: => String): Unit = {
+            queue.offer1(Some(StdErr(s))).unsafeRunAsyncAndForget()
+          }
+
+          def buffer[T](f: => T): T = f
         }
+
+        Process(commands).run(processLogger).exitValue()
+      }.flatMap(code => queue.offer1(Some(Exit(code)))) *> queue.offer1(None)).start
     } yield queue.dequeue.unNoneTerminate
 
+    Stream.eval(streamIO).flatten
+  }
+
+  def toEitherT(stream: Stream[IO, ProgramEvent]): EitherT[IO, String, String] = EitherT {
+    stream.compile.toList.map {
+      case events :+ Exit(0) => Right(events.collect({
+        case StdOut(line) => line
+      }).mkString)
+      case events => Left(events.collect({
+        case StdErr(line) => line
+      }).mkString)
+    }
   }
 
 }
